c70227391d24766706dc79ecce930f0c744e4f27
Mauricio Armenio <mauricioarmenio@gmail.com>
2019-10-02 00:18:51 -0300

    agregado el cambio de estado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     nombre: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({nombre: event.target.value});
+  }
+
+  render() {
+    return (
+    <div className="contenedor">
+      <form>
+        <label>Ingresá tu nombre</label>
+        <input
+          type="text"
+          name="nombre"
+          value={this.state.nombre} 
+          onChange={this.handleChange}
+        />
+      </form>
+      <div className="ver-nombre">
+      <h3>Tu nombre es: {this.state.nombre}</h3>
     </div>
-  );
+    </div>
+    );
+  }
 }
 
-export default App;
+export default Form;
